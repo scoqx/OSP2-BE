5b03134601e404e7d076de3154a8ac58af360ca8
diwoc <diwoc@proton.me>
2025-04-22 02:34:10 +0300

    Revert "Header test"
    This reverts commit aa360cc1661ccd97f78b7a3a0cb7dfbbfd442719.

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -2247,7 +2247,7 @@ int CG_NewParticleArea(int num);
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
 // #define OSP_VERSION "0.06-test" // OSP2 ogirinal
-#define OSP_VERSION "be-0.05733" // BE
+#define OSP_VERSION "be-0.057" // BE
 
 
 
