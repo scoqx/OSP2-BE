e854afd69a18e12af77da575de4ae1c5a1b5e688
diwoc <diwoc@proton.me>
2025-05-03 14:44:45 +0300

    0.06c3
    Team text width scaled down (80% -> 75% of height)

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -2294,7 +2294,7 @@ int CG_NewParticleArea(int num);
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
 // #define OSP_VERSION "0.06-test" // OSP2 ogirinal
-#define OSP_VERSION "be-0.06c2" // BE
+#define OSP_VERSION "be-0.06c3" // BE
 
 
 
