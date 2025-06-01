a66f98d5e011a641598d64380f6585f57a5fd0f9
diwoc <diwoc@proton.me>
2025-06-01 17:46:03 +0300

    be-v0.07c
    net icon fix

--- assets/scripts/osp2.shader
+++ assets/scripts/osp2.shader
@@ -537,6 +537,6 @@ disconnectedNew
 		map gfx/2d/net.tga
 		blendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
         depthWrite
-		rgbGen identity
+		rgbGen vertex
 	}
 }
--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -2318,7 +2318,7 @@ int CG_NewParticleArea(int num);
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
 // #define OSP_VERSION "0.06-test" // OSP2 ogirinal
-#define OSP_VERSION "be-0.07b" // BE
+#define OSP_VERSION "be-0.07c" // BE
 
 
 
