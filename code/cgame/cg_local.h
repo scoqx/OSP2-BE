266737f7e2afb146d9eb936c67535bf902299a7e
diwoc <diwoc@proton.me>
2025-07-14 15:19:24 +0300

    be-v0.086a
    updated mirage huds

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -2346,7 +2346,7 @@ int CG_NewParticleArea(int num);
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
 // #define OSP_VERSION "0.06-test" // OSP2 ogirinal
-#define OSP_VERSION "be-0.086" // BE
+#define OSP_VERSION "be-0.086a" // BE
 
 
 
