96900fb0760288f12dda22cfd59d04579ba6a6f8
diwoc <diwoc@proton.me>
2025-09-01 15:18:30 +0300

    blyat

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -2489,7 +2489,7 @@ int CG_NewParticleArea(int num);
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
 // #define OSP_VERSION "0.06-test" // OSP2 ogirinal
-#define OSP_VERSION "be-0.94b" // BE
+#define OSP_VERSION "be-0.94c" // BE
 #define OSP_CLIENT_VERSION "1008_OSP2"
 
 
